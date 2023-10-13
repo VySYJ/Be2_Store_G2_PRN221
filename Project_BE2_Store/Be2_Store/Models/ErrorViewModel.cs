0046f85c1c0de83d0a5cc2d8362506ab481780e0
Vyltt-25 <vyltt.work@gmail.com>
2023-10-13 10:55:32 +0700

    push

---
+++ Project_BE2_Store/Be2_Store/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Be2_Store.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
